2f6506abb2eaba885e88850734df8db6fdba80ba
Jane Venter <janeanneventer@gmail.com>
2018-12-02 18:59:04 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,4 +1,18 @@
 module Findable 
-  def find_by_name 
+  @@all = []
+ 
+  # attr_accessor :name
+ 
+  # def initialize(name)
+  #   @name = name
+  # end
+ 
+  def all
+    @@all
   end
+ 
+  def find_by_name(name)
+    @@all.detect {|a| a.name == name}
+  end
+  
 end
--- lib/song.rb
+++ lib/song.rb
@@ -3,23 +3,24 @@ require 'pry'
 class Song
   
   extend Memorable
+  extend Findable
   
-  attr_accessor :name
+  #attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  # @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
-  def self.all
-    @@songs
-  end
+  # def self.all
+  #   @@songs
+  # end
 
   # def self.reset_all
   #   self.all.clear
